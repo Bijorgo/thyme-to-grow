5bf0e8cb47faecd0e130fc4cdfd5fb4a31164431
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-02-26 10:04:34 -0700

    add testing data to seed

--- server/seed.py
+++ server/seed.py
@@ -7,7 +7,8 @@ def seed_plants():
     plants = [
         Plant(name="Thyme", level=1),
         Plant(name="Carrot", level=2),
-        Plant(name="Tulip", level=2)
+        Plant(name="Tulip", level=2),
+        #Plant(name="True Tester", level=1) # for testing true option in field guide 
     ]
     db.session.add_all(plants)
     db.session.commit()
@@ -28,19 +29,40 @@ def seed_cultivated():
     # This is for dev testing only
     print("seeding cultivated plants is for dev testing only. If this was not intended, please stop and adjust.")
     print("seeding cultivated plants...")
-    cultivated = []
-    print(f"{len(cultivated)} entries seeded!")
+    plants_from_db = Plant.query.all()
+    if not plants_from_db:
+        print("no plants available to seed, skipping seeding...")
+        return
+    gardens_from_db = Garden.query.all()
+    if not gardens_from_db:
+        print("no gardens available to seed, skipping seeding...")
+        return
+    cultivated_entries = []
+    # assign every plant to every garden, tested and works
+    #for garden in gardens_from_db:
+    #    for plant in plants_from_db: # both a garden id and plant id are required to create an entry
+    #        cultivated_entries.append(CultivatePlants(plant_id=plant.id, garden_id=garden.id))
+    #db.session.add_all(cultivated_entries)
+    #db.session.commit()
+    print(f"{len(cultivated_entries)} entries seeded!")
 
 def seed_field_guide():
     print("seeding field guide...")
     plants = Plant.query.all()
     if not plants:
-        print("No plants found, skipping field guide seeding...")
+        print("No plants found, skipping seeding...")
         return
-    field_guide_entires = [FieldGuide(status=False, plant_id=plant.id) for plant in plants]
-    db.session.add_all(field_guide_entires)
+    field_guide_entries = []
+
+    for plant in plants:
+        if plant.name == "True Tester":
+            field_guide_entries.append(FieldGuide(plant_id=plant.id, status=True))
+        else:
+            field_guide_entries.append(FieldGuide(status=False, plant_id=plant.id))
+
+    db.session.add_all(field_guide_entries)
     db.session.commit()
-    print(f"{len(field_guide_entires)} entries seeded!")
+    print(f"{len(field_guide_entries)} entries seeded!")
 
 def seed_players():
     print("seeding players...")
@@ -63,6 +85,7 @@ def seed_database():
         # Drop existing tables and create new ones
         print("dropping all tables...")
         db.drop_all()  
+        print("creating all tables...")
         db.create_all()
         
         # Seed the database with data
